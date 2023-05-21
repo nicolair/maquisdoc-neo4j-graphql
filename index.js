7ad151e74e6508952cb667c6b3c7768ca17858bd
Rémy <remy.nicolai@gmail.com>
2023-05-21 12:24:35 +0200

    forcer le rebuild 2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Neo4jGraphQL } = require("@neo4j/graphql");
 const { ApolloServer, gql } = require("apollo-server");
 const neo4j = require("neo4j-driver");
-
+// test rebuild
 
 const typeDefs = gql`
 type Document {
