c34b6f027ff746cf2fe4eede4bb5021274761191
Rémy <remy.nicolai@gmail.com>
2025-11-06 10:01:10 +0100

    Changement syntaxe requetes graphql suite mise à jour  Sur la branche main  Votre branche est à jour avec 'origin/main'.
     Modifications qui seront validées :
    	modifié :         index.js
    	modifié :         package-lock.json
    	modifié :         package.json

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const { Neo4jGraphQL } = require("@neo4j/graphql");
-const { ApolloServer, gql } = require("apollo-server");
-const neo4j = require("neo4j-driver");
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { Neo4jGraphQL } from "@neo4j/graphql";
+import neo4j  from "neo4j-driver";
 // test rebuild
+// dans l'ancienne v : typeDefs gql ??
 
-const typeDefs = gql`
-type Document {
-    _id: ID! @cypher(statement: "RETURN ID(this)") 
+
+const typeDefs = `#graphql
+enum OrdAlpha {
+  ASC
+  DESC
+}
+
+input TriDocument {
+  titre: OrdAlpha
+  description: OrdAlpha
+}
+
+type Document @node {
+    _id: ID! @cypher(
+      statement: "RETURN ID(this) AS ident",
+      columnName: "ident") 
     titre: String
     typeDoc: String
     description: String
@@ -40,19 +55,24 @@ type ConceptVoisin {
     conceptId: ID!
 }
 
-type Concept {
+type Concept @node {
     litteral: String
     discipline : String
     description: String
-    _id: ID! @cypher(statement: "RETURN ID(this)") 
+    _id: ID! @cypher(
+      statement: "RETURN ID(this) AS id",
+      columnName: "id") 
     documents: [Document!]! @relationship(type: "DOCUMENTE", direction: IN)
-    listexos: [Document!]! @cypher(statement: """
-      MATCH (f {typeDoc: \\"liste exercices\\"})-[:EVALUE]->(this)
-      RETURN f
-    """)
-    documentsvoisins: [DocumentVoisin] @cypher(statement: """ 
-      MATCH (this:Concept)-[r]-(d:Document)
-      WITH {
+    listexos: [Document!]! @cypher(
+      statement: """
+        MATCH (f {typeDoc: 'liste exercices'})-[:EVALUE]->(this)
+        RETURN f AS liste
+      """,
+      columnName:"liste")
+    documentsvoisins: [DocumentVoisin] @cypher(
+      statement: """ 
+        MATCH (this:Concept)-[r]-(d:Document)
+        WITH {
            typeRel: type(r) ,
            out: id(endNode(r))=id(d) ,
            docType : d.typeDoc ,
@@ -60,69 +80,90 @@ type Concept {
            docDescription: d.description,
            docUrl: d.url,
            docId: id(d)
-      } AS DocumentVoisin
-      RETURN DocumentVoisin
-    """)
-    conceptsvoisins: [ConceptVoisin] @cypher(statement: """ 
-      MATCH (this:Concept)-[r]-(c:Concept)
-      WITH {
+        } AS DocumentVoisin
+        RETURN DocumentVoisin AS docvois
+      """,
+      columnName: "docvois")
+    conceptsvoisins: [ConceptVoisin] @cypher(
+      statement: """ 
+        MATCH (this:Concept)-[r]-(c:Concept)
+        WITH {
            typeRel: type(r) ,
            out: id(endNode(r))=id(c) ,
            conceptLitteral : c.litteral ,
            conceptDescription: c.description,
            conceptId: id(c)
-      } AS ConceptVoisin
-      RETURN ConceptVoisin
-    """)
+        } AS ConceptVoisin
+        RETURN ConceptVoisin AS concvois
+      """,
+      columnName: "concvois")
 }
 
-type Evenement {
+type Evenement @node {
     nom: String 
     typeEvt: String
-    _id: ID! @cypher(statement: "RETURN ID(this)") 
+    _id: ID! @cypher(
+      statement: "RETURN ID(this) AS ident",
+      columnName: "ident") 
     concepts: [Concept!]! @relationship(type: "EVALUE", direction: OUT)
     documents: [Document!]! @relationship(type: "UTILISE", direction: OUT)
     sousevenements: [Evenement!]! @relationship(type: "CONTIENT", direction: OUT)
 }
 
 type Query {
-  feuilleexercicesdocuments : [Document!]! @cypher(statement: """
-    MATCH (d:Document {typeDoc:\\"liste exercices\\"})
-    RETURN d
-  """),
-  exercicedocuments : [Document!]! @cypher(statement: """
-    MATCH (d:Document {typeDoc:\\"exercice\\"})
-    RETURN d
-  """),
-  coursdocuments : [Document!]! @cypher(statement: """
-    MATCH (d:Document {typeDoc:\\"cours\\"})
-    RETURN d
-  """),
-  problemedocuments : [Document!]! @cypher(statement: """
-    MATCH (d:Document {typeDoc:\\"problème\\"})
-    RETURN d
-  """),
-  semaines : [Evenement!]! @cypher(statement: """
-    MATCH (s:Evenement {typeEvt:\\"semaine de colle\\"})
-    RETURN s
-  """),
-  searchpbs (mot:String): [Document!] @cypher(statement: """
-    CALL db.index.fulltext.queryNodes(\\"TitresEtDescriptions\\", $mot)
-      YIELD node, score
-    WHERE node.typeDoc = \\"problème\\"
-    RETURN  node
-  """),
-  searchcours (mot:String): [Document!] @cypher(statement: """
-    CALL db.index.fulltext.queryNodes(\\"TitresEtDescriptions\\", $mot)
-      YIELD node, score
-    WHERE node.typeDoc = \\"cours\\"
-    RETURN  node
-  """),
-  searchconcepts (mot:String): [Concept!] @cypher(statement: """
-    CALL db.index.fulltext.queryNodes(\\"LittérauxEtDescriptions\\", $mot)
-      YIELD node
-    RETURN  node
-  """)
+  feuilleexercicesdocuments : [Document!]! @cypher(
+    statement: """
+      MATCH (d:Document {typeDoc:'liste exercices'})
+      RETURN d AS listexos
+    """,
+    columnName : "listexos"),
+  exercicedocuments : [Document!]! @cypher(
+    statement: """
+      MATCH (d:Document {typeDoc:'exercice'})
+      RETURN d AS exodoc
+    """,
+    columnName: "exodoc")
+  coursdocuments : [Document!]! @cypher(
+      statement: """
+        MATCH (d:Document {typeDoc:'cours'})
+        RETURN d AS coursdoc
+      """,
+      columnName: "coursdoc")
+  problemedocuments : [Document!]! @cypher(
+    statement: """
+      MATCH (d:Document {typeDoc:'problème'})
+      RETURN d AS pbdoc
+    """,
+    columnName: "pbdoc")
+  semaines : [Evenement!]! @cypher(
+    statement: """
+      MATCH (s:Evenement {typeEvt:'semaine de colle'})
+      RETURN s AS evt
+    """,
+    columnName: "evt")
+  searchpbs (mot:String): [Document!] @cypher(
+    statement: """
+      CALL db.index.fulltext.queryNodes('TitresEtDescriptions', $mot)
+        YIELD node, score
+        WHERE node.typeDoc = 'problème'
+      RETURN  node AS titredescrp
+    """,
+    columnName: "titredescrp")
+  searchcours (mot:String): [Document!] @cypher(
+    statement: """
+      CALL db.index.fulltext.queryNodes('TitresEtDescriptions', $mot)
+        YIELD node, score
+        WHERE node.typeDoc = 'cours'
+      RETURN  node AS mot
+    """,
+    columnName: "mot")
+  searchconcepts (mot:String): [Concept!] @cypher(
+    statement: """
+      CALL db.index.fulltext.queryNodes('LittérauxEtDescriptions', $mot)
+        YIELD node
+      RETURN  node AS mot
+    """,
+    columnName: "mot")
 }
 `;
 
@@ -135,15 +176,24 @@ const driver = neo4j.driver(
     neo4j.auth.basic(neo4j_username,neo4j_pw)
     );
 
-
 const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
 
-neoSchema.getSchema().then((schema) => {
-  const server = new ApolloServer({
-      schema,
-  });
+const server = new ApolloServer({
+  schema:await neoSchema.getSchema(),
+});
 
-  server.listen(3003,"0.0.0.0").then(({ url }) => {
-      console.log(`Server ready at ${url}`);
-  });
-})
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 3003},
+});
+
+console.log(`Server ready at ${url}`);
+
+//neoSchema.getSchema().then((schema) => {
+//  const server = new ApolloServer({
+//      schema,
+//  });
+
+//  server.listen(3003,"0.0.0.0").then(({ url }) => {
+//      console.log(`Server ready at ${url}`);
+//
+//})
